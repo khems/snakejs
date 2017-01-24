722945d05c90bbcbbb00fefc682fba44641eb84b
Aimee Wyld <aimee.eloise@mail.com>
2017-01-24 10:16:48 +0000

    Added event listener for arrow keys

--- main.js
+++ main.js
@@ -3,4 +3,24 @@ document.addEventListener('DOMContentLoaded', function(){
 
   //returns a 2d drawing context on the canvas
   var ctx = canvas.getContext('2d')
+
+  //event listener for arrow keys
+  document.addEventListener('keydown', function(e) {
+      var key = e.keyCode
+      switch(key) {
+          case 37:
+          //move left
+              break
+          case 38:
+          //move down
+              break
+          case 39:
+         //move right
+              break
+          case 40:
+          //move up
+              break
+      }
+  })
+
 })

4a9c54658173507fb2340539adbd5f5debc75957
Khem <khem@mayden.co.uk>
2017-01-24 12:14:46 +0000

    add movement and interval

--- main.js
+++ main.js
@@ -3,24 +3,48 @@ document.addEventListener('DOMContentLoaded', function(){
 
   //returns a 2d drawing context on the canvas
   var ctx = canvas.getContext('2d')
+  
+  setInterval(function(){drawSnakeParts(ctx, snake)}, 1000)
+  
+  //snake
+  var snake = new snakePart(ctx, 10, 10)
 
   //event listener for arrow keys
   document.addEventListener('keydown', function(e) {
       var key = e.keyCode
-      switch(key) {
-          case 37:
-          //move left
-              break
-          case 38:
-          //move down
-              break
-          case 39:
-         //move right
-              break
-          case 40:
-          //move up
-              break
-      }
+      console.log(key)
+      moveSnakePart(key, snake)
+    //  drawSnakeParts(ctx, snake)
   })
-
 })
+
+function moveSnakePart(direction, snakePart){
+  switch (direction){
+    case 37: 
+      snakePart.xpos -= 10
+      console.log('left')
+      break
+    case 38: 
+      snakePart.ypos -= 10
+      console.log('up')
+      break
+    case 39: 
+      snakePart.xpos += 10
+      console.log('right')
+      break
+    case 40: 
+      snakePart.ypos += 10
+      console.log('down')
+      break
+  }
+}
+
+function drawSnakeParts(ctx, snakeparts){
+  ctx.clearRect(0,0,300,150)
+  ctx.fillRect(snakeparts.xpos, snakeparts.ypos, 10, 10)
+}
+
+function snakePart(ctx, xpos, ypos){
+  this.xpos = xpos
+  this.ypos = ypos
+}
